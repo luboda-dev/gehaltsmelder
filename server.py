d6f36b47d3229b66a4545f75212a34a143947aae
luboda-dev <58166916+luboda-dev@users.noreply.github.com>
2025-10-08 22:57:26 +0200

    Update server.py

--- server.py
+++ server.py
@@ -58,3 +58,8 @@ def report():
     except Exception as e:
         print("Fehler beim Senden:", e)
         return jsonify({"success": False, "error": str(e)}), 500
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
